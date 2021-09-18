3b0a779fd829f929b74c814f49a8f7dae1bedb6d
rmbziiik <77121871+rmbziiik@users.noreply.github.com>
2021-09-18 14:11:18 +0600

    Add files via upload

---
+++ Homework#1_ Loops_and_Functions.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	for i := 0; i < 10; i++ {
+		z = z - (z*z-x)/(2*z)
+	}
+	return z
+}
+
+func main() {
+	i := float64(81)
+	fmt.Println(Sqrt(i), Sqrt(i) == math.Sqrt(i))
+}
